b9f04028a649ff861c2018b666c32e2da1e6a73b
Jan Dvorak <jan.dvorak@ff.cuni.cz>
2021-02-03 09:11:27 +0100

    add support for basic authentication

--- download2.py
+++ download2.py
@@ -4,6 +4,7 @@
 # It will store them in-place: a subsequent `git status` should not find any difference.
 #
 import requests
+from requests.auth import HTTPBasicAuth
 import os
 import pathlib
 import re
@@ -13,12 +14,16 @@ url_base = "https://api.eurocris.org/dris"
 input_output_dir = "data"
 limit_dir = "/entries"
 
+auth_env_var_name = "DRIS_CREDENTIALS"
+my_credentials = os.environ[auth_env_var_name].split( ':' ) if auth_env_var_name in os.environ else None
+my_auth = HTTPBasicAuth( my_credentials[0], my_credentials[1] ) if auth_env_var_name in os.environ else None
+
 s = requests.Session()
 n_files = 0
 for x in sorted( pathlib.Path( input_output_dir + limit_dir ).rglob( "*.json" ), key=os.path.getmtime ):
     url = url_base + re.sub( '^' + input_output_dir, '', re.sub( r'\.json$', '', x.as_posix() ) )
     print( url )
-    r = s.get( url, stream=True, headers = { "Accept": "application/json" } )
+    r = s.get( url, stream=True, headers = { "Accept": "application/json" }, auth=my_auth )
     r.raise_for_status()
     ct = r.headers["Content-Type"]
     with open( x, "w" ) as f:
